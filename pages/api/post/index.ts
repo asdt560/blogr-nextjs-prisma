41ba87df06a07fd1dd7a864ebd0572725f31a910
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-06-09 17:55:22 -0300

    Fix errors

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,12 +1,13 @@
 import { getServerSession } from "next-auth/next"
 import prisma from '../../../lib/prisma';
+import {options} from '../auth/[...nextauth]'
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
-  const session = await getServerSession(req);
+  const session = await getServerSession(req, res, options);
   const result = await prisma.post.create({
     data: {
       title: title,
